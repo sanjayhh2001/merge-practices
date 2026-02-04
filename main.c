43a200e3691026ec6b554793c8ce48e5b63d148c
Sanjay H H <sanjayhh2001@gmail.com>
2026-02-04 13:43:27 +0530

    math-opration

---
+++ main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int add(int a,int b);
+int sub(int a,int b);
+
+int main()
+{
+    int num1,num2;
+
+    printf("Enter the two values: ");
+    scanf("%d %d",&num1,&num2);
+
+    int Sum = add(num1,num2);
+    int Sub = add(num1,num2);
+
+    printf("The addition is: %d",Sum);
+    printf("The Subtraction is: %d",Sub);
+
+    return 0;
+}
